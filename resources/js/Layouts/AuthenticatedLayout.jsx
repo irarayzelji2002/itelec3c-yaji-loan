090a7c055bcb62a96dd16b5a3a03593e6a8095b0
aliahestbn <aliah.esteban.cics@ust.edu.ph>
2024-12-17 19:53:26 +0800

    admin dashboard

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,9 +1,9 @@
-import ApplicationLogo from "@/Components/ApplicationLogo";
 import Dropdown from "@/Components/Dropdown";
 import NavLink from "@/Components/NavLink";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
 import { Link, usePage } from "@inertiajs/react";
 import { useState } from "react";
+import "../Pages/App.css";
 
 export default function AuthenticatedLayout({ header, children }) {
   const user = usePage().props.auth.user;
@@ -12,18 +12,22 @@ export default function AuthenticatedLayout({ header, children }) {
 
   return (
     <div className="min-h-screen bg-gray-100">
-      <nav className="border-b border-gray-100 bg-white">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="flex h-16 justify-between">
+      <nav className="nav-bar-logged">
+        <div className="nav-bar-logged mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="nav-bar-logged flex h-16 justify-between">
             <div className="flex">
               <div className="flex shrink-0 items-center">
                 <Link href="/">
-                  <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                  <img className="block h-12 w-auto" src="/img/logosText.png" alt="Workflow" />
                 </Link>
               </div>
 
               <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                <NavLink href={route("dashboard")} active={route().current("dashboard")}>
+                <NavLink
+                  style={{ fontWeight: "aliceblue" }}
+                  href={route("dashboard")}
+                  active={route().current("dashboard")}
+                >
                   Dashboard
                 </NavLink>
               </div>
